134cf2c557f8d40ad88079435b2931a1c08dfad5
yanz <dirtysalt1987@gmail.com>
2024-09-19 06:59:23 +0800

    update files

--- scripts/auto-update.sh
+++ scripts/auto-update.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
 # Copyright (C) dirlt
 
+git pull
+f=/tmp/mysite-auto-update.txt
 cur_cmt=`git rev-parse HEAD`
-last_cmt=`cat /tmp/mysite-auto-update.txt`
-echo $cur_cmt > /tmp/mysite-auto-update.txt
-if [ $cur_cmt != $last_cmt ]; then
+last_cmt=`cat $f`
+echo $cur_cmt > $f
+if [ "$cur_cmt" != "$last_cmt" ]; then
     bash ./scripts/publish.sh
+    if [ $? != 0 ]; then
+        echo "failed" > $f
+    fi
 fi
